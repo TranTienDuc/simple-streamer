44293f29f413cbbb3d34971b961904dfae27d741
TranTienDuc <trantienduc@gmail.com>
2025-04-23 09:14:15 +0700

    Update app_streamer.py

--- app_streamer.py
+++ app_streamer.py
@@ -1,46 +1,23 @@
-import threading
-import cv2
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration, RTCIceServer
 
-st.set_page_config(page_title="Webcam", layout="wide")
-       
+# Cấu hình TURN và STUN
+rtc_config = RTCConfiguration(
+    iceServers=[
+        RTCIceServer(urls=["stun:stun.l.google.com:19302"]),
+        RTCIceServer(
+            urls=["turn:openrelay.metered.ca:80"],
+            username="openrelayproject",
+            credential="openrelayproject"
+        )
+    ]
+)
 
-col1, col2 = st.columns(2)
+# Streamlit UI
+st.title("💻 Streamlit WebRTC với TURN server")
 
-lock = threading.Lock()
-img_container = {"img": None}
-
-
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    with lock:
-        img_container["img"] = img
-    return
-
-with col1:
-    ctx = webrtc_streamer(key="example", 
-                          video_frame_callback=video_frame_callback,
-
-                          rtc_configuration={  # Add this config
-                            "iceServers": [
-                                {"urls": ["turn:openrelay.metered.ca:443"],
-                                 "username": "openrelayproject",
-                                 "credential": "openrelayproject"
-                                 }
-                                ]
-                            },
-                          media_stream_constraints={"video": True, "audio": False})
-
-imgout_place = col2.empty()
-
-while ctx.state.playing:
-    with lock:
-        img = img_container["img"]
-    if img is None:
-        continue
-    # Phát hiện khuôn mặt sẽ đặt ở đây
-    # Kết quả trả về là imgout có đóng khung khuôn mặt
-    imgout = cv2.flip(img, 0)
-
-    imgout_place.image(imgout, channels='BGR')
+webrtc_streamer(
+    key="example",
+    rtc_configuration=rtc_config,
+    media_stream_constraints={"video": True, "audio": True},
+)
